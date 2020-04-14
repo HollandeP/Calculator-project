e0a163a36c1b4153a33175d85a5f55b99c4bfb7b
Hollande <hollandepowell@gmail.com>
2020-04-14 19:08:59 -0400

    Grid Layout setup, buttons instantiated

--- app/src/main/java/com/example/gridcalculator/MainActivity.java
+++ app/src/main/java/com/example/gridcalculator/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.gridcalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button clear = findViewById(R.id.clear);
+        Button clearEverything = findViewById(R.id.clearEverything);
+        Button plus = findViewById(R.id.plus);
+        Button minus = findViewById(R.id.minus);
+        Button multiply = findViewById(R.id.multiply);
+        Button divide = findViewById(R.id.divide);
+        Button decimal = findViewById(R.id.decimal);
+        Button equals = findViewById(R.id.equals);
+        Button zero = findViewById(R.id.zero);
+        Button one = findViewById(R.id.one);
+        Button two = findViewById(R.id.two);
+        Button three = findViewById(R.id.three);
+        Button four = findViewById(R.id.four);
+        Button five = findViewById(R.id.five);
+        Button six = findViewById(R.id.six);
+        Button seven = findViewById(R.id.seven);
+        Button eight = findViewById(R.id.eight);
+        Button nine = findViewById(R.id.nine);
     }
 }
